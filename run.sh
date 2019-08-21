7f0d6509cb186c2dab58879f5e24d6aaaed93939
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-08-21 12:10:44 +0200

    Add run script for testing

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker run \
+  -u root \
+  --rm \
+  -d \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v archilab-jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --name archilab-jenkins \
+  jenkins/jenkins:lts-alpine
+
+docker exec -it archilab-jenkins apk add docker
