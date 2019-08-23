5ec67a90b6c47e6461de94e82e264afad64aeaa3
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-08-23 14:45:05 +0200

    Remove commented code from run script

--- run.sh
+++ run.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-#docker run \
-#  -u root \
-#  --rm \
-#  -d \
-#  -p 8080:8080 \
-#  -p 50000:50000 \
-#  -v archilab-jenkins-data:/var/jenkins_home \
-#  -v /var/run/docker.sock:/var/run/docker.sock \
-#  --name archilab-jenkins \
-#  jenkins/jenkins:lts-alpine
-
-#docker exec -it archilab-jenkins apk add docker
-
 docker build -t archilab-jenkins .
 
 docker stack deploy -c docker-compose.yml archilab-jenkins
