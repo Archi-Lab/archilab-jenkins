a7cfdef9b533d8e02bb9bf1990df68fa7f7fc254
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-08-23 14:58:45 +0200

    Add script for Docker secret creation

---
+++ create-secrets.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+docker secret create GITHUB_TOKEN ./secrets/GITHUB_TOKEN
+docker secret create GITHUB_USERNAME ./secrets/GITHUB_USERNAME
+docker secret create GITHUB_PASSWORD ./secrets/GITHUB_PASSWORD
+docker secret create NEXUS_USERNAME ./secrets/NEXUS_USERNAME
+docker secret create NEXUS_PASSWORD ./secrets/NEXUS_PASSWORD
+docker secret create PROX_PROD_CERTS_SECRET ./secrets/PROX_PROD_CERTS_SECRET
+docker secret create PROX_PROD_CERTS_CLIENT ./secrets/PROX_PROD_CERTS_CLIENT
+docker secret create PROX_PROD_CERTS_CA_SERVER ./secrets/PROX_PROD_CERTS_CA_SERVER
+docker secret create PROX_DEV_CERTS_SECRET ./secrets/PROX_DEV_CERTS_SECRET
+docker secret create PROX_DEV_CERTS_CLIENT ./secrets/PROX_DEV_CERTS_CLIENT
+docker secret create PROX_DEV_CERTS_CA_SERVER ./secrets/PROX_DEV_CERTS_CA_SERVER
+docker secret create COALBASE_DEV_CERTS_SECRET ./secrets/COALBASE_DEV_CERTS_SECRET
+docker secret create COALBASE_DEV_CERTS_CLIENT ./secrets/COALBASE_DEV_CERTS_CLIENT
+docker secret create COALBASE_DEV_CERTS_CA_SERVER ./secrets/COALBASE_DEV_CERTS_CA_SERVER
+docker secret create COALBASE_DEV_CERTS_SECRET ./secrets/COALBASE_DEV_CERTS_SECRET
+docker secret create COALBASE_DEV_CERTS_CLIENT ./secrets/COALBASE_DEV_CERTS_CLIENT
+docker secret create COALBASE_DEV_CERTS_CA_SERVER ./secrets/COALBASE_DEV_CERTS_CA_SERVER
