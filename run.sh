afd0d6029fc277ce803c36bf8e68e1bdcf7c9ecf
Rudolf Grauberger <rudolf@grauberger.org>
2019-09-11 14:39:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-COMPOSE_PROJECT_NAME="archilab-jenkins"
+compose_project_name="archilab-jenkins"
 
-docker build --pull -t $COMPOSE_PROJECT_NAME .
+docker build --pull -t $compose_project_name .
 
-docker stack rm $COMPOSE_PROJECT_NAME
+docker stack rm $compose_project_name
 
-LIMIT=15
-until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$LIMIT" -lt 0 ]; do
+limit=15
+until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$compose_project_name -q)" ] || [ "$limit" -lt 0 ]; do
   sleep 1;
-  LIMIT="$((LIMIT-1))"
+  limit="$((limit-1))"
 done
 
-LIMIT=15
-until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$LIMIT" -lt 0 ]; do
+limit=15
+until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$compose_project_name -q)" ] || [ "$limit" -lt 0 ]; do
   sleep 1;
-  LIMIT="$((LIMIT-1))"
+  limit="$((limit-1))"
 done
 
-docker stack deploy -c docker-compose.yml $COMPOSE_PROJECT_NAME
+docker stack deploy -c docker-compose.yml $compose_project_name
