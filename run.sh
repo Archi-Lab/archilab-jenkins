04778dd6aa60eb348c5e160dab0749f7881ce2a7
Rudolf Grauberger <rudolf@grauberger.org>
2019-09-11 13:21:01 +0200

    Waits until the service & network are deleted
    Avoids problem reported in https://github.com/moby/moby/issues/29293
    
    Uses the solution from https://github.com/moby/moby/issues/29293#issuecomment-318852549

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-docker build --pull -t archilab-jenkins .
+COMPOSE_PROJECT_NAME="archilab-jenkins"
+docker build --pull -t $COMPOSE_PROJECT_NAME .
 
-docker stack rm archilab-jenkins
+docker stack rm $COMPOSE_PROJECT_NAME
 
-docker stack deploy -c docker-compose.yml archilab-jenkins
+until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$limit" -lt 0 ]; do
+  sleep 1;
+done
+
+until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$limit" -lt 0 ]; do
+  sleep 1;
+done
+
+docker stack deploy -c docker-compose.yml $COMPOSE_PROJECT_NAME
