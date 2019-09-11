5fcc5aad84e5fa1f6b01ea057e7542a783c50b4b
Rudolf Grauberger <rudolf@grauberger.org>
2019-09-11 13:54:49 +0200

    Fixes issue with limit

--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 COMPOSE_PROJECT_NAME="archilab-jenkins"
-LIMIT=10
+
 docker build --pull -t $COMPOSE_PROJECT_NAME .
 
 docker stack rm $COMPOSE_PROJECT_NAME
 
+LIMIT=15
 until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$LIMIT" -lt 0 ]; do
   sleep 1;
+  LIMIT="$((LIMIT-1))"
 done
 
+LIMIT=15
 until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$COMPOSE_PROJECT_NAME -q)" ] || [ "$LIMIT" -lt 0 ]; do
   sleep 1;
+  LIMIT="$((LIMIT-1))"
 done
 
 docker stack deploy -c docker-compose.yml $COMPOSE_PROJECT_NAME
