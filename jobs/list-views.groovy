19c402ef4c15ca1f2b409f53dc368859929eceb7
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-14 12:24:18 +0100

    Add job for list view creation

---
+++ jobs/list-views.groovy
@@ -0,0 +1,44 @@
+listView('prox') {
+    jobs {
+        regex('prox.*')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+
+listView('coalbase') {
+    jobs {
+        regex('coalbase.*')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+
+listView('fae') {
+    jobs {
+        regex('fae.*')
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
