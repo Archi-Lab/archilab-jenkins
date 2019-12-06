6fe729bfbc0f88bc8c359f6a18c5e108fba17235
JannIntveen <JannIntveen@gmail.com>
2019-12-06 14:37:35 +0100

    add fae draussen ortung pipeline

---
+++ jobs/fae-draussen-ortung.groovy
@@ -0,0 +1,20 @@
+pipelineJob('fae-draussen-ortung') {
+    definition {
+        cpsScm {
+            scriptPath 'src/main/jenkins/Jenkinsfile'
+            scm {
+                git {
+                    remote {
+                        github('Archi-Lab-FAE/fae-draussen-ortung')
+                        credentials('archilab-github-jenkins')
+                    }
+                    branches('master')
+                }
+            }
+            lightweight(true)
+        }
+    }
+    triggers {
+        githubPush()
+    }
+}
