6ae532e84bb291235e453c17ae9a26533cdeea5a
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2020-02-22 19:20:24 +0100

    Update compose file version

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-compose_project_name="archilab-jenkins"
+project_name="archilab-jenkins"
 
-docker build --pull -t $compose_project_name .
+docker build --pull -t $project_name .
 
-docker stack rm $compose_project_name
+docker stack rm $project_name
 
 limit=15
-until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$compose_project_name -q)" ] || [ "$limit" -lt 0 ]; do
-  sleep 1;
-  limit="$((limit-1))"
+until [ -z "$(docker service ls --filter label=com.docker.stack.namespace=$project_name -q)" ] || [ "$limit" -lt 0 ]; do
+  sleep 1
+  limit="$((limit - 1))"
 done
 
 limit=15
-until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$compose_project_name -q)" ] || [ "$limit" -lt 0 ]; do
-  sleep 1;
-  limit="$((limit-1))"
+until [ -z "$(docker network ls --filter label=com.docker.stack.namespace=$project_name -q)" ] || [ "$limit" -lt 0 ]; do
+  sleep 1
+  limit="$((limit - 1))"
 done
 
-docker stack deploy -c docker-compose.yml $compose_project_name
+docker stack deploy -c docker-compose.yml $project_name
