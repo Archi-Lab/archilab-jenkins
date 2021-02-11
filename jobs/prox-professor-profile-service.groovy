5e231d20356ee6f57a1cf02e74b89858dee640fa
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-02-11 15:53:58 +0100

    Add prox-professor-profile-service

---
+++ jobs/prox-professor-profile-service.groovy
@@ -0,0 +1,32 @@
+multibranchPipelineJob('prox-professor-profile-service') {
+    branchSources {
+        branchSources {
+            branchSource {
+                source {
+                    github {
+                        apiUri('https://api.github.com')
+                        configuredByUrl(true)
+                        credentialsId('archilab-github-jenkins-token-username-password')
+                        repoOwner('innovation-hub-bergisches-rheinland')
+                        repository('prox-professor-profile-service')
+                        repositoryUrl('https://github.com/innovation-hub-bergisches-rheinland/prox-professor-profile-service')
+                        traits {
+                            gitHubBranchDiscovery {
+                                strategyId(1)
+                            }
+                            headWildcardFilter {
+                                includes('dev master')
+                                excludes('')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('src/main/jenkins/Jenkinsfile')
+        }
+    }
+}
