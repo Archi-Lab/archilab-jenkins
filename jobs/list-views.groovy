dbab9b14beba1bcf456abfbb196ec09b7b816f53
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2021-06-27 13:05:02 +0200

    Remove obsolete FAE pipeline jobs

--- jobs/list-views.groovy
+++ jobs/list-views.groovy
@@ -12,33 +12,3 @@ listView('prox') {
         buildButton()
     }
 }
-
-// listView('coalbase') {
-//     jobs {
-//         regex('coalbase.*')
-//     }
-//     columns {
-//         status()
-//         weather()
-//         name()
-//         lastSuccess()
-//         lastFailure()
-//         lastDuration()
-//         buildButton()
-//     }
-// }
-
-listView('fae') {
-    jobs {
-        regex('fae.*')
-    }
-    columns {
-        status()
-        weather()
-        name()
-        lastSuccess()
-        lastFailure()
-        lastDuration()
-        buildButton()
-    }
-}
