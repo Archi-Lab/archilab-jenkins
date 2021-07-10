0e4d481ce8e8da95aaa77dca090ea514661027cc
Tobias Hund <tobiashund6112+git@gmail.com>
2021-07-10 15:35:15 +0200

    Add prox-job-service

---
+++ jobs/prox-job-service.groovy
@@ -0,0 +1,32 @@
+multibranchPipelineJob('prox-job-service') {
+    branchSources {
+        branchSources {
+            branchSource {
+                source {
+                    github {
+                        apiUri('https://api.github.com')
+                        configuredByUrl(true)
+                        credentialsId('archilab-github-jenkins-token-username-password')
+                        repoOwner('innovation-hub-bergisches-rheinland')
+                        repository('prox-job-service')
+                        repositoryUrl('https://github.com/innovation-hub-bergisches-rheinland/prox-job-service')
+                        traits {
+                            gitHubBranchDiscovery {
+                                strategyId(1)
+                            }
+                            headWildcardFilter {
+                                includes('dev master')
+                                excludes('')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('src/main/jenkins/Jenkinsfile')
+        }
+    }
+}
