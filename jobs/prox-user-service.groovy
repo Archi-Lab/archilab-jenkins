c7d2ffdfae4416e17a70e3b883179505d9a31d20
Tobias Hund <tobiashund6112+git@gmail.com>
2021-10-07 14:24:15 +0200

    Add User Service

---
+++ jobs/prox-user-service.groovy
@@ -0,0 +1,32 @@
+multibranchPipelineJob('prox-user-service') {
+    branchSources {
+        branchSources {
+            branchSource {
+                source {
+                    github {
+                        apiUri('https://api.github.com')
+                        configuredByUrl(true)
+                        credentialsId('archilab-github-jenkins-token-username-password')
+                        repoOwner('innovation-hub-bergisches-rheinland')
+                        repository('prox-user-service')
+                        repositoryUrl('https://github.com/innovation-hub-bergisches-rheinland/prox-user-service')
+                        traits {
+                            gitHubBranchDiscovery {
+                                strategyId(1)
+                            }
+                            headWildcardFilter {
+                                includes('dev main')
+                                excludes('')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('src/main/jenkins/Jenkinsfile')
+        }
+    }
+}
