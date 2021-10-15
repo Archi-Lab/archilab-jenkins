6b65e4e4076f7d5348f599e9180011e727a45c21
Tobias Hund <tobiashund6112+git@gmail.com>
2021-10-15 17:14:27 +0200

    Add Schedule check

--- init/2_run-all-jobs.groovy
+++ init/2_run-all-jobs.groovy
@@ -3,7 +3,9 @@ import hudson.model.*;
 jobs = Hudson.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)
 
 for (job in jobs) {
-    if (job instanceof BuildableItem) {
+  if(job.isBuildable()) {
+    if (!job.isBuilding() && !job.isInQueue()) {
       job.scheduleBuild();
     }
+  }
 }
