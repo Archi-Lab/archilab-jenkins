bb1d29feded48059ebdae773a6c4b96a69372037
Tobias Hund <tobiashund6112+git@gmail.com>
2021-10-15 17:00:44 +0200

    Remove no trigger property

--- init/1_scan-all-multibranch-pipelines.groovy
+++ init/1_scan-all-multibranch-pipelines.groovy
@@ -6,11 +6,5 @@ for (f in Jenkins.instance.getAllItems(jenkins.branch.MultiBranchProject.class))
   if (f.parent instanceof jenkins.branch.OrganizationFolder) {
     continue;
   }
-  for (s in f.sources) {
-    def prop = new jenkins.branch.NoTriggerBranchProperty();
-    def propList = [prop] as jenkins.branch.BranchProperty[];
-    def strategy = new jenkins.branch.DefaultBranchPropertyStrategy(propList);
-    s.setStrategy(strategy);
-  }
   f.computation.run();
 }
